7b7704df5dd35abf24b042b5dfed148f324177ad
Daniela Hayashi <107941355+danchih@users.noreply.github.com>
2024-06-14 17:24:01 -0300

    Add files via upload

---
+++ VeiculoRoubado.vue
@@ -0,0 +1,177 @@
+
+<template>
+<div class="container mx-auto" style="background-color: #dcdcdc; width: 900px;">
+
+    <br>
+    <p class="uppercase text-center text-4xl font-bold ">
+        Principais dicas a serem seguidas ao ter seu Veiculo Roubado
+    </p>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa1.png" alt="placa1">
+            </div>
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Ligue imediatamente para a polícia
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Ligue imediatamente para o 190 e informe à Polícia Militar sobre o ocorrido. Quanto mais rápido for o contato, maiores serão as chances de localizar e recuperar o veículo.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Registre um boletim de ocorrência
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Registre um boletim de ocorrência para informar o que aconteceu. Esta medida é essencial para a produção de provas, garantindo proteção à vítima, que tem um carro registrado em seu nome na posse de criminosos.
+                </p>
+            </div>
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa2.png" alt="placa2">
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa3.png" alt="placa3">
+            </div>
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Procure dar detalhes ao fazer o registro
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Procure fornecer detalhes ao registrar o boletim de ocorrência, como o horário e o local exato do roubo, além das características do veículo levado. Em São Paulo, boletins de ocorrência para furto e roubo de veículos podem ser feitos online, por meio da Delegacia Eletrônica.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Acione a seguradora do carro, se tiver contratado
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Entre em contato com a seguradora do veículo, se houver um seguro contratado. Geralmente, será necessário apresentar o boletim de ocorrência, documentos pessoais e do carro (ou cópias destes) para acionar a cobertura. A dinâmica pode variar conforme a seguradora.
+                </p>
+            </div>
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa4.png" alt="placa4">
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa5.png" alt="placa5">
+            </div>
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Comunique o roubo ou furto do veículo à PRF
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Informe o roubo ou furto do veículo à PRF (Polícia Rodoviária Federal). O órgão disponibiliza o serviço Sinal (Sistema Nacional de Alarmes), que emite um alerta para a corporação sobre ocorrências desse tipo de crime
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Mantenha os documentos em local de fácil acesso
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Mantenha os documentos em local de fácil acesso (em nuvem, ou guardado de uma outra forma).
+                </p>
+            </div>
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa6.png" alt="placa6">
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa7.png" alt="placa7">
+            </div>
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Evite fazer postagens sobre o roubo na internet
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Evite compartilhar informações sobre o roubo nas redes sociais. Isso pode incentivar golpes e atividades fraudulentas.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Peça restituição do IPVA, em casos em que o imposto já foi pago
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Conforme o site do Detran-SP, se o IPVA do veículo roubado ou furtado foi pago, o proprietário pode solicitar a restituição do imposto pago ao Estado.
+                </p>
+            </div>
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa8.png" alt="placa8">
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+        <div class="md:flex">
+            <div class="shrink-0">
+                <img class="object-contain h-48 w-96" src="/img/placas/placa9.png" alt="placa9">
+            </div>
+            <div class="p-8">
+                <div class="uppercase tracking-wide text-base text-indigo-500 font-bold" style="color: #ea0000;">
+                    Entre em contato com o Detran para dar baixa do veículo
+                </div>
+                <p class="mt-2 text-black text-lg">
+                    Caso o veículo não seja recuperado após alguns dias, é crucial contatar o Detran para realizar a baixa do veículo. Isso garante que ele seja removido do cadastro nacional de veículos e não possa mais circular legalmente.
+                </p>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="max-w-md mx-auto overflow-hidden md:max-w-2xl">
+        <p class="uppercase text-base text-black font-bold"> Referência Bibliográfica:</p>
+        <a href="https://www.estadao.com.br/sao-paulo/teve-o-carro-roubado-entenda-o-que-fazer-a-seguir-nprm/#:~:text=Para%20isso%2C%20basta%20acessar%20o,partir%20de%20um%20telefone%20fixo." class="block mt-1 text-lg leading-tight font-medium text-red-500 hover:underline">Notícia Estadão (São Paulo) - Teve o carro roubado? Entenda o que fazer a seguir</a>
+    </div>      
+
+
+    <br>
+</div>
+
+
+
+</template>
+  
+
